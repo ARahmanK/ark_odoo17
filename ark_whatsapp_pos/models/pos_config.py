21bd2bf56201325b54fc9e9de36af51843dc42b1
ARK <rumankhan7438@gmail.com>
2025-05-05 14:35:52 +0500

    add custom module

---
+++ ark_whatsapp_pos/models/pos_config.py
@@ -0,0 +1,11 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class PosConfig(models.Model):
+    _inherit = 'pos.config'
+
+    whatsapp_enabled = fields.Boolean('WhatsApp Enabled', default=False)
+    receipt_template_id = fields.Many2one('mail.template', string="Receipt template", domain=[('model', '=', 'pos.order'), ('is_chatroom_template', '=', True)])
+    invoice_template_id = fields.Many2one('mail.template', string="Invoice template", domain=[('model', '=', 'account.move'), ('is_chatroom_template', '=', True)])
